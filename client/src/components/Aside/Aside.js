5e5203aee076d03868dc42b9bdaae89590693cbe
mhudyx <mhudyx@gmail.com>
2021-08-15 20:15:12 +0200

    Add styles for aside and content components

--- client/src/components/Aside/Aside.js
+++ client/src/components/Aside/Aside.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import './Aside.scss';
 
 const Aside = () => {
     return (
         <aside>
-            aside
+            {/* logo do wklejenia na potem */}
+            <div className="banner">MHDEV</div>
+            <div className="navigation">
+                <div className="navigation-el">Dashboard</div>
+                <div className="navigation-el">Projects</div>
+                <div className="navigation-el">Tasks</div>
+                <div className="navigation-el">Reports</div>
+                <div className="navigation-el">Manage</div>
+                <div className="navigation-el">Account</div>
+            </div>   
         </aside>
     )
 }
