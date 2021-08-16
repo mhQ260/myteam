217a02647fff74eb077a5ed847031a38b30afe88
mhudyx <mhudyx@gmail.com>
2021-08-16 12:02:03 +0200

    Add logo for aside

--- client/src/components/Aside/Aside.js
+++ client/src/components/Aside/Aside.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './Aside.scss';
+import Logo from '../../img/logo.svg';
 
 const Aside = () => {
     return (
         <aside>
-            {/* logo do wklejenia na potem */}
-            <div className="banner">MHDEV</div>
             <div className="navigation">
                 <div className="navigation-el">Dashboard</div>
                 <div className="navigation-el">Projects</div>
@@ -14,6 +13,7 @@ const Aside = () => {
                 <div className="navigation-el">Manage</div>
                 <div className="navigation-el">Account</div>
             </div>   
+            <div className="banner"><img src={Logo} alt="Logo" /></div>
         </aside>
     )
 }
