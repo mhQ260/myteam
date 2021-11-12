9c9eed5135c2bb8e443a41a75798d0bb0a00503d
mhudyx <mhudyx@gmail.com>
2021-11-12 11:35:34 +0100

    Add manage page with components

--- client/src/pages/Manage/Manage.js
+++ client/src/pages/Manage/Manage.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './Manage.scss';
 
 const ManagePage = () => {
     return (
-        <>
-                manage page
-        </>
+        <div className="manage">
+            <div className="manage-header"><h1>Manage Panel</h1></div>
+            <div className="manage-container">
+                <Link to="/manage/users" className="navigation-el"><div><i class="fas fa-users"></i> Users</div></Link>
+                <Link to="/manage/projects" className="navigation-el"><div><i class="fas fa-project-diagram"></i> Projects</div></Link>
+                <Link to="/manage/clients" className="navigation-el"><div><i class="fas fa-handshake"></i> Clients</div></Link>
+                <Link to="/manage/support" className="navigation-el"><div> [soon]</div></Link>
+                <Link to="/manage/settings" className="navigation-el"><div><i class="fas fa-cogs"></i> Settings</div></Link>
+                <Link to="/manage/support" className="navigation-el"><div><i class="fas fa-headset"></i> Support</div></Link>
+                <Link to="/manage/support" className="navigation-el"><div> [soon]</div></Link>
+                <Link to="/manage/support" className="navigation-el"><div><i class="fas fa-info"></i> About</div></Link>
+            </div>
+        </div>
     )
 }
 
