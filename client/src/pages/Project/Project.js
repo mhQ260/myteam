043934c4c7a8197134ca3f1a9d611a731f4902ec
mhudyx <mhudyx@gmail.com>
2021-12-09 12:41:24 +0100

    Add navigation for mobile version project page

--- client/src/pages/Project/Project.js
+++ client/src/pages/Project/Project.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, Component} from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import './Project.scss';
@@ -7,6 +7,10 @@ import { Doughnut } from 'react-chartjs-2';
 
 const ProjectPage = (props) => {
 
+    const [isActiveOverview, setActiveOverview] = useState(false);
+    const [isActiveTasks, setActiveTasks] = useState(false);
+    const [isActiveUsers, setActiveUsers] = useState(false);
+
     const projectDetails = useSelector(state => state.projectDetails);
     const { loading, project, error } = projectDetails;
     const dispatch = useDispatch();
@@ -14,6 +18,53 @@ const ProjectPage = (props) => {
     useEffect(() => {
         dispatch(detailsProject(props.match.params.id));
     }, [props.match.params.id]);
+
+    const toggleClass = (n) => {
+
+        if(n == "a") {
+            setActiveOverview(true);
+            setActiveTasks(false);
+            setActiveUsers(false);
+        } else if (n == "b") {
+            setActiveOverview(false);
+            setActiveTasks(true);
+            setActiveUsers(false);
+        } else if(n == "c") {
+            setActiveOverview(false);
+            setActiveTasks(false);
+            setActiveUsers(true);
+        }
+    };
+
+    class OverwiewComponent extends React.Component {
+        render () {
+          return(
+            <div>
+              Overview area
+            </div>
+          )
+        }
+    }
+
+    class TasksComponent extends React.Component {
+        render () {
+          return(
+            <div>
+              Tasks area
+            </div>
+          )
+        }
+    }
+
+    class UsersComponent extends React.Component {
+        render () {
+          return(
+            <div>
+              Users area
+            </div>
+          )
+        }
+    }
 
 
     return <div className="project">
@@ -24,7 +75,7 @@ const ProjectPage = (props) => {
             <div className="project-summary">
                 <div className="summary-diagram">
                     <div>
-                            
+                                
                     </div>
                     <p>Tasks done</p>
                 </div>
@@ -34,12 +85,13 @@ const ProjectPage = (props) => {
                 </div>
             </div>
             <div className="project-content">
-                Recent activity
+                {isActiveOverview ? <OverwiewComponent /> : isActiveTasks ? <TasksComponent /> : isActiveUsers ? <UsersComponent /> : null}
+               
             </div>
             <div className="nav">
-                <div>Overview</div>
-                <div>Tasks</div>
-                <div>Users</div>
+                <div className={isActiveOverview ? 'active-border' : null} onClick={() => toggleClass("a")}>Overwiew</div>
+                <div className={isActiveTasks ? 'active-border' : null} onClick={() => toggleClass("b")}>Tasks</div>
+                <div className={isActiveUsers ? 'active-border' : null} onClick={() => toggleClass("c")}>Users</div>
             </div>
         </>
         }
