80f6224295925d11b7148b47f51eb97fd56db385
mhudyx <mhudyx@gmail.com>
2021-12-14 12:51:38 +0100

    Remake model for team member into a project member

--- server/models/projectMember.model.js
+++ server/models/projectMember.model.js
@@ -1,11 +1,15 @@
 import mongoose from 'mongoose';
 
 const projectMemberSchema = new mongoose.Schema({
-    role: { type: Number, required: true },
-    project_id: { type: Schema.Types.ObjectId, ref: 'project' },
-    user_id: { type: Schema.Types.ObjectId, ref: 'user' },
+    project_id: { type: mongoose.Schema.Types.ObjectId, ref: 'project' },
+    user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'user' },
+    role: { type: Number, required: true }    
 })
 
-const projectMemberModel = mongoose.model("projectMember", projectMemberSchema);
+const projectMemberModel = mongoose.model("ProjectMember", projectMemberSchema);
 
 export default projectMemberModel;
+
+
+// project_id: { type: String, ref: 'project' },
+//     user_id: { type: String, ref: 'user' },
