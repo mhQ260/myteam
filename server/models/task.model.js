50cc74de9737b86482f34b61182d21a517277952
mhudyx <mhudyx@gmail.com>
2021-12-16 20:16:51 +0100

    Minor changes in task model

--- server/models/task.model.js
+++ server/models/task.model.js
@@ -1,15 +1,15 @@
 import mongoose from 'mongoose';
 
 const TaskSchema = new mongoose.Schema({
-    summary: { type: String, required: true, unique: true },
-    projet_id: { type: mongoose.Schema.Types.ObjectId, ref: 'project' },
+    summary: { type: String, required: true },
+    project_id: { type: mongoose.Schema.Types.ObjectId, ref: 'project' },
     assigne_user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'user' },
     create_user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'user' },
     created_at: { type: Date, default: Date.now, required: true },
     updated_at: { type: Date },
     description: { type: String, required: true },
     priority: { type: Number, required: true },
-    status: { type: Number, default: 1, required: true },
+    status: { type: Number, default: 1 },
     deadline: { type: Date, required: true },
     progress: { type: Number, default: 0 }
 })
