cee458dd9f4fd148c1ddc4095127b2e4b057e8c1
mhudyx <mhudyx@gmail.com>
2021-12-18 15:16:46 +0100

    Change links for stats and about pages

--- client/src/pages/Manage/Manage.js
+++ client/src/pages/Manage/Manage.js
@@ -10,11 +10,10 @@ const ManagePage = () => {
                 <Link to="/manage/users" className="navigation-el"><div><i class="fas fa-users"></i> Users</div></Link>
                 <Link to="/manage/projects" className="navigation-el"><div><i class="fas fa-project-diagram"></i> Projects</div></Link>
                 <Link to="/manage/clients" className="navigation-el"><div><i class="fas fa-handshake"></i> Clients</div></Link>
-                <Link to="/manage/support" className="navigation-el"><div> [soon]</div></Link>
+                <Link to="/manage/stats" className="navigation-el"><div><i class="fas fa-chart-bar"></i> Stats</div></Link>
                 <Link to="/manage/settings" className="navigation-el"><div><i class="fas fa-cogs"></i> Settings</div></Link>
-                <Link to="/manage/support" className="navigation-el"><div><i class="fas fa-headset"></i> Support</div></Link>
-                <Link to="/manage/support" className="navigation-el"><div> [soon]</div></Link>
-                <Link to="/manage/support" className="navigation-el"><div><i class="fas fa-info"></i> About</div></Link>
+                <Link to="/manage/support" className="navigation-el"><div><i class="fas fa-headset"></i> Support</div></Link> 
+                <Link to="/manage/about" className="navigation-el"><div><i class="fas fa-info"></i> About</div></Link>
             </div>
         </div>
     )
