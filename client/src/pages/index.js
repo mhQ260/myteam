49f452c2b6d4c8e36113d15de2a52acce3c4c88c
mhudyx <mhudyx@gmail.com>
2022-01-27 18:27:20 +0100

    Changes in page imports

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,8 +2,11 @@ export { default as AccountPage } from './Account';
 export { default as DashboardPage } from './Dashboard';
 export { default as ManagePage } from './Manage';
 export { default as ProjectsPage } from './Projects';
+export { default as ProjectPage } from './Project';
 export { default as ReportsPage } from './Reports';
 export { default as TasksPage } from './Tasks';
+export { default as TaskPage } from './Task';
 export { default as SigninPage } from './Signin';
 export { default as UsersManagePage } from './UsersManage';
 export { default as ProjectsManagePage } from './ProjectsManage';
+
